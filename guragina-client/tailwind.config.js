0be76ded1cdda1089d10fd069787ba2ed7b1df9f
k4l3b-plo <kaleba647@gmail.com>
2023-04-10 13:49:06 +0300

    updated tailwind config

--- guragina-client/tailwind.config.js
+++ guragina-client/tailwind.config.js
@@ -1,16 +1,27 @@
+const { colors } = require('@mui/material');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        "text-base": "12px"
       },
+      colors: {
+        main: "#1C8EC2",
+
+        bgLight: "#E6E6E6",
+        fgLight: "#FFFFFF",
+
+        bgDark: "#858585",
+        fgDark: "#2A2B2E",
+
+      }
     },
   },
   plugins: [],
