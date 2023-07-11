9358af81d732a593fe0ecbc9832de5f08a69b393
CalebBlion <kaleba647@gmail.com>
2023-07-11 12:54:54 +0300

    Register the Post model

--- lesan_api/Posts/admin.py
+++ lesan_api/Posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Post
+
 # Register your models here.
+
+
+@admin.register(Post)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "content",
+        "created_at",
+        "edited_at",
+        )
