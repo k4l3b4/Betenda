b12ac4d4088da2ecf7c5594c1f16ea63a0b9da22
CalebBlion <kaleba647@gmail.com>
2023-07-11 12:55:33 +0300

    Register all models in the Users app

--- lesan_api/Users/admin.py
+++ lesan_api/Users/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
+from .models import Device, Invitation, Password_reset, User
+
 # Register your models here.
+
+
+@admin.register(User)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "user_name",
+        "phone_number",
+        "is_active",
+        "is_superuser",
+    )
+
+@admin.register(Invitation)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "invitation_code",
+        "disabled",
+        "count",
+    )
+
+@admin.register(Password_reset)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "otp",
+        "created_at",
+        "expires_at",
+    )
+
+@admin.register(Device)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "ip_address",
+        "device_type",
+        "city",
+        "country",
+        "logged_in",
+    )
