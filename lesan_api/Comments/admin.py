ae8888721369e2d95f9d1fbbcfd8aba7b0978c3d
CalebBlion <kaleba647@gmail.com>
2023-07-11 12:56:48 +0300

    Registered Comments

--- lesan_api/Comments/admin.py
+++ lesan_api/Comments/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Comments
+
 # Register your models here.
+
+
+@admin.register(Comments)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "comment",
+        "created_at",
+        )
