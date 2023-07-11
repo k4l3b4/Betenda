458f5c31520144016285cb84952fcaed0724a758
CalebBlion <kaleba647@gmail.com>
2023-07-11 12:54:35 +0300

    Register MileStoneType, MileStone models

--- lesan_api/MileStones/admin.py
+++ lesan_api/MileStones/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import MileStone, MileStoneType
+
 # Register your models here.
+
+
+@admin.register(MileStoneType)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "milestone_name",
+        "milestone_type",
+        )
+    
+@admin.register(MileStone)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "milestone",
+        "achievement_date",
+        )
+
