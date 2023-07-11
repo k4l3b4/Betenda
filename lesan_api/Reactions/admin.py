f37d063c6e79731dcd9de14b002fa60a69effa5a
CalebBlion <kaleba647@gmail.com>
2023-07-11 12:55:12 +0300

    Register Reaction model

--- lesan_api/Reactions/admin.py
+++ lesan_api/Reactions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Reaction
+
 # Register your models here.
+
+
+@admin.register(Reaction)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "reaction",
+        "created_at",
+        )
