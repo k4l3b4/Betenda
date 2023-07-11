ca38f081f799511c666d8e24a0465812e5a7a8e2
CalebBlion <kaleba647@gmail.com>
2023-07-11 12:53:34 +0300

    Register all the models(Contrib) in the admin ui interface

--- lesan_api/Contribs/admin.py
+++ lesan_api/Contribs/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
 
+from .models import Poem, Saying, Sentence, TargetLanguage, SourceLanguage, Word
+
 # Register your models here.
+
+
+@admin.register(SourceLanguage)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "language",
+        "iso_code",
+        "glottolog_code",
+        "created_at",
+        "edited_at",
+    )
+
+
+@admin.register(TargetLanguage)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "language",
+        "iso_code",
+        "glottolog_code",
+        "created_at",
+        "edited_at",
+    )
+
+
+@admin.register(Word)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "word",
+        "source_language",
+        "target_language",
+        "created_at",
+    )
+
+
+@admin.register(Poem)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "language",
+        "created_at",
+        "edited_at",
+    )
+
+
+@admin.register(Saying)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "language",
+        "created_at",
+        "edited_at",
+    )
+
+
+@admin.register(Sentence)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "sentence",
+        "source_language",
+        "target_language",
+        "created_at",
+    )
