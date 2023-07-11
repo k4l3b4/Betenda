d16b4610a96de0b4304158c8282639c8da243335
CalebBlion <kaleba647@gmail.com>
2023-07-12 02:17:47 +0300

    Added the token refresh view

--- lesan_api/Users/urls.py
+++ lesan_api/Users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import LoginView, UserCreateView
 
 
 urlpatterns = [
     path('register/<str:code>', UserCreateView.as_view(), name="create_account_view"),
-    path('login', LoginView.as_view(), name="login_view")
+    path('login', LoginView.as_view(), name="login_view"),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
 ]
