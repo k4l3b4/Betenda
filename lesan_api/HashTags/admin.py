6fe94f7732a3ead34f76b662993ed6103e309595
CalebBlion <kaleba647@gmail.com>
2023-07-11 12:54:02 +0300

    Register HashTag,Engagement model in the admin ui interface

--- lesan_api/HashTags/admin.py
+++ lesan_api/HashTags/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Engagement, HashTag
+
 # Register your models here.
+
+
+@admin.register(HashTag)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "tag",
+        "created_date",
+    )
+
+@admin.register(Engagement)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "tag",
+        "date",
+        "duration",
+        "int_amount",
+    )
