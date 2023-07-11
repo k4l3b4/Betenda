754d96fdf72a3a021ee03ac2424d51654d9dfb2c
CalebBlion <kaleba647@gmail.com>
2023-07-11 13:00:54 +0300

    Updated, minor fixes, comments

--- lesan_api/Users/models.py
+++ lesan_api/Users/models.py
@@ -10,7 +10,7 @@ from Users.fields import IntegerRangeField
 
 class CustomAccountManager(BaseUserManager):
 
-    def create_superuser(self, email, first_name, last_name, password, **other_fields):
+    def create_superuser(self, email, user_name, first_name, last_name, password, **other_fields):
 
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
@@ -23,11 +23,11 @@ class CustomAccountManager(BaseUserManager):
             raise ValueError(
                 'Superuser must be assigned as a superuser.')
 
-        return self.create_user(email, first_name, last_name, password, **other_fields)
+        return self.create_user(email, user_name, first_name, last_name, password, **other_fields)
 
-    def create_user(self, email, first_name, last_name, password, **other_fields):
+    def create_user(self, email, user_name, first_name, last_name, password, **other_fields):
 
-        user = self.model(email=email,
+        user = self.model(email=email, user_name=user_name,
                           first_name=first_name, last_name=last_name, **other_fields)
         user.set_password(password)
         user.save()
@@ -35,15 +35,12 @@ class CustomAccountManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    '''
+    The default User model
+    '''
     class SEX(models.TextChoices):
         MALE = "MALE", "Male"
         FEMALE = "FEMALE", "Female"
-
-    id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    sex = models.CharField(
-        _("Sex"), max_length=50, choices=SEX.choices, default=None, blank=True, null=True)
-    invitation_code = models.CharField(_("Invitation code"), max_length=10, blank=True, null=True)
     email = models.EmailField(
         _("Email"), max_length=254, unique=True, default=None, blank=False, null=True)
     first_name = models.CharField(
@@ -52,6 +49,9 @@ class User(AbstractBaseUser, PermissionsMixin):
         _("Last name"), max_length=150, blank=False, null=True)
     user_name = models.CharField(
         _("User name"), max_length=50, blank=False, null=True)
+    bio = models.CharField(_("Bio"), max_length=255, blank=False, null=True)
+    sex = models.CharField(
+        _("Sex"), max_length=50, choices=SEX.choices, default=None, blank=True, null=True)
     profile_avatar = models.ImageField(
         _("Profile photo"), upload_to='user/Users/profile_pic', max_length=None, blank=False, null=True)
     birth_date = models.DateField(
@@ -76,6 +76,32 @@ class User(AbstractBaseUser, PermissionsMixin):
         related_name='groups'
     )
 
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'user_name', 'last_name']
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+
+class Invitation(models.Model):
+    '''
+    A users invitation code fields: user, invitation_code, count
+    '''
+    user = models.ForeignKey("User", verbose_name=_(
+        "User"), on_delete=models.CASCADE)
+    invitation_code = models.CharField(
+        _("Invitation code"), max_length=10, blank=True, null=True)
+    count = models.PositiveIntegerField(
+        _("Invited users count"), blank=True, null=False)
+    disabled = models.BooleanField(_("Disabled"), default=False)
+
+    class Meta:
+        verbose_name = _("Invitation code")
+        verbose_name_plural = _("Invitation codes")
+
 
 class Password_reset(models.Model):
     """
@@ -95,20 +121,22 @@ class Password_reset(models.Model):
     @property
     def expired(self):
         """
-        returns a boolean value based on weather the time has passed now
+        returns a boolean value based on weather the time has passed timezone.now()
         """
         return self.expires_at < timezone.now()
 
     class Meta:
-        verbose_name = _("Password reset")
-        verbose_name_plural = _("Password reset")
-        
+        verbose_name = _("Password reset OTP")
+        verbose_name_plural = _("Password reset OTPS")
+
 
 class Device(models.Model):
     user = models.ForeignKey("User", verbose_name=_(
         "User"), on_delete=models.CASCADE, blank=False, null=True)
     ip_address = models.CharField(
         _("Ip address"), max_length=255, blank=False, null=True)
+    device_id = models.CharField(
+        _("Device Id"), max_length=255, blank=False, null=True)
     device_type = models.CharField(
         _("Device Type"), max_length=255, blank=False, null=True)
     device_name = models.CharField(
@@ -120,3 +148,7 @@ class Device(models.Model):
     operating_system = models.CharField(
         _("Operating system"), max_length=255, blank=False, null=True)
     logged_in = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = _("Device")
+        verbose_name_plural = _("Devices")
