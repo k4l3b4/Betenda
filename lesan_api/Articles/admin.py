b8f44a5c9be81bb6df9c5a891474a1bf328e3347
CalebBlion <kaleba647@gmail.com>
2023-07-11 12:53:05 +0300

    Register Article model in the admin ui interface

--- lesan_api/Articles/admin.py
+++ lesan_api/Articles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from Articles.models import Article
+
 # Register your models here.
+
+
+@admin.register(Article)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "published_date",
+        "modified_date",
+        )
