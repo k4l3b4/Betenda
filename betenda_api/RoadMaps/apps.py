3fd33fabd131a023154abf790defcf69e55c9f49
CalebBlion <kaleba647@gmail.com>
2023-07-12 16:44:13 +0300

    minor change

--- betenda_api/RoadMaps/apps.py
+++ betenda_api/RoadMaps/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RoadmapConfig(AppConfig):
+class RoadmapsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'RoadMap'
+    name = 'RoadMaps'
