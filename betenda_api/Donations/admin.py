5643e87541229367b3d46a67b84103661d033e13
CalebBlion <kaleba647@gmail.com>
2023-07-12 16:37:29 +0300

    Registered the Campaign model

--- betenda_api/Donations/admin.py
+++ betenda_api/Donations/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from Donations.models import Campaign
+
 # Register your models here.
+
+
+@admin.register(Campaign)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "reason",
+        "amount_needed",
+        "amount_donated",
+        "campaign_start",
+        "campaign_end",
+    )
