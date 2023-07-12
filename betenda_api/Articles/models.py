48d9dc0a8df8aed7fbfeedb6228f420f4247d711
CalebBlion <kaleba647@gmail.com>
2023-07-12 16:45:15 +0300

    added status, featured fields

--- betenda_api/Articles/models.py
+++ betenda_api/Articles/models.py
@@ -4,8 +4,13 @@ from django.utils.translation import gettext as _
 
 # Create your models here.
 class Article(models.Model):
+    STATUS = (
+        ('DRAFT', 'Draft'),
+        ('PUBLISHED', 'Published'),
+    )
+
     title = models.CharField(
-        _("Article title"), max_length=150, blank=False, null=True)
+        _("Article title"), max_length=255, blank=False, null=True)
     slug = models.SlugField(
         _("Article slug"), default="", blank=False, null=False)
     desc = models.CharField(_("Article description"),
@@ -14,10 +19,12 @@ class Article(models.Model):
     image = models.ImageField(
         _("Article image"), upload_to='article/images/', max_length=None, blank=True, null=True)
     authors = models.ManyToManyField("Users.User", verbose_name=_("Authors"))
+    status = models.CharField(_("Status"), choices=STATUS, max_length=255)
+    featured = models.BooleanField(_("Featured"), default=False)
     published_date = models.DateTimeField(
         _("Published date"), auto_now=False, auto_now_add=True)
     modified_date = models.DateTimeField(
         _("modified date"), auto_now=True, auto_now_add=False)
-    
+
     def __str__(self):
         return f"{self.title[:20]}.., by {self.authors.first()}"
