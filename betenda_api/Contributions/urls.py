c8c024257f08f66710d620e3c3e9794dde52fdf0
CalebBlion <kaleba647@gmail.com>
2023-07-12 12:45:19 +0300

    Added the views for the appropriate url

--- betenda_api/Contributions/urls.py
+++ betenda_api/Contributions/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import Language_CUD_APIView, Word_CUD_APIView
+from .views import Language_CUD_APIView, Saying_CUD_APIView, Sentence_CUD_APIView, Poem_CUD_APIView, Word_CUD_APIView
 
 urlpatterns = [
-    path('lang', Language_CUD_APIView.as_view(), name="language_add_update_delete_view"),
-    path('word', Word_CUD_APIView.as_view(), name="word_add_update_delete_view"),
-    path('poem', Word_CUD_APIView.as_view(), name="poem_add_update_delete_view"),
-    path('saying', Word_CUD_APIView.as_view(), name="saying_add_update_delete_view"),
-    path('sentence', Word_CUD_APIView.as_view(), name="sentence_add_update_delete_view"),
+    path('lang', Language_CUD_APIView.as_view(),
+         name="language_add_update_delete_view"),
+    path('word', Word_CUD_APIView.as_view(),
+         name="word_add_update_delete_view"),
+    path('poem', Poem_CUD_APIView.as_view(),
+         name="poem_add_update_delete_view"),
+    path('saying', Saying_CUD_APIView.as_view(),
+         name="saying_add_update_delete_view"),
+    path('sentence', Sentence_CUD_APIView.as_view(),
+         name="sentence_add_update_delete_view"),
 ]
