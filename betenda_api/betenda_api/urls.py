635da4260afc1b7b814e45d9ecb6c01139f64864
CalebBlion <kaleba647@gmail.com>
2023-07-13 21:46:08 +0300

    Added media url for development

--- betenda_api/betenda_api/urls.py
+++ betenda_api/betenda_api/urls.py
@@ -1,17 +1,19 @@
-"""
-URL configuration for lesan_api project.
-"""
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('Users.urls')),
     path('api/contributions/', include('Contributions.urls')),
-    path('api/posts/', include('Users.urls')),
+    path('api/posts/', include('Posts.urls')),
     path('api/comments/', include('Users.urls')),
     path('api/articles/', include('Users.urls')),
     path('api/hashtags/', include('Users.urls')),
     path('api/reactions/', include('Users.urls')),
     path('api/tags/', include('Users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
