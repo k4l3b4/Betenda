27a8e72c4b141570041fba7f2227ef4bc354a47d
CalebBlion <kaleba647@gmail.com>
2023-07-13 21:49:14 +0300

    Added Donation model

--- betenda_api/Donations/models.py
+++ betenda_api/Donations/models.py
@@ -18,3 +18,21 @@ class Campaign(models.Model):
     class Meta:
         verbose_name = _("Campaign")
         verbose_name_plural = _("Campaigns")
+
+
+class Donation(models.Model):
+    user = models.ForeignKey(
+        "Users.User", verbose_name=_("Donator"), on_delete=models.CASCADE)
+    donation_amount = models.PositiveIntegerField(_("Donation amount"))
+    donation_for = models.ForeignKey("Campaign", verbose_name=_(
+        "Donation cause"), blank=True, null=True, on_delete=models.CASCADE)
+    remark = models.CharField(_("Remark"), max_length=255)
+    donation_date = models.DateTimeField(
+        _("Donation date"), auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.donation_amount} for {self.donation_for} on {self.donation_date}"
+
+    class Meta:
+        verbose_name = _("Donation")
+        verbose_name_plural = _("Donations")
