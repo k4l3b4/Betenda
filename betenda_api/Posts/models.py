751638168e35956430a794af52da140234199c9f
CalebBlion <kaleba647@gmail.com>
2023-07-14 22:46:59 +0300

    refactor: :card_file_box: add hashtags, reactions,edited_at fields and a process_hashtags method
    Added a hashtags field to relate tags to the post instance added reactions to allow user reactions for a post instance and a process_hashtags method that will procces the content field and save/relate the tags in the hashtags field/relation

--- betenda_api/Posts/models.py
+++ betenda_api/Posts/models.py
@@ -1,33 +1,60 @@
+import re
 from django.db import models
 from django.utils.translation import gettext as _
-from django.utils.text import slugify
+from django_extensions.db.fields import AutoSlugField
+from django.contrib.contenttypes.fields import GenericRelation
+from HashTags.models import HashTag
+
 
 class Post(models.Model):
     class TYPE(models.TextChoices):
         IMAGE = "IMAGE", "Image"
         VIDEO = "VIDEO", "Video"
         AUDIO = "AUDIO", "Audio"
-        
-    user = models.ForeignKey("Users.User", verbose_name=_("User"), on_delete=models.PROTECT, blank=True, null=True)
-    content = models.CharField(_("Content"), max_length=280)
-    parent = models.ForeignKey('self', verbose_name=_("Replied to"), related_name="post_parent", on_delete=models.CASCADE, null=True, blank=True)
-    slug = models.SlugField(
-        _("Slug"), default="", blank=False, null=False)
+
+    user = models.ForeignKey("Users.User", verbose_name=_(
+        "User"), on_delete=models.PROTECT, blank=True, null=True)
+    content = models.TextField(_("Content"))
+    parent = models.ForeignKey('self', verbose_name=_(
+        "Replied to"), related_name="post_parent", on_delete=models.CASCADE, null=True, blank=True)
+    slug = AutoSlugField(_("Slug"), populate_from=['title'])
     media = models.FileField(_("Media"), upload_to='post/media/',
                              max_length=None, blank=True, null=True)
+    hashtags = models.ManyToManyField(
+        "HashTags.HashTag", blank=True)
+    reactions = GenericRelation("Reactions.Reaction")
     media_type = models.CharField(
         _("Media type"), max_length=20, choices=TYPE.choices, null=True, blank=True)
     created_at = models.DateTimeField(_("Created date"), auto_now_add=True)
-    edited_at = models.DateTimeField(_("Edited date"), blank=True, null=True, auto_now=True)
+    edited_at = models.DateTimeField(
+        _("Edited date"), blank=True, null=True, auto_now=True)
 
     def __str__(self):
         return f"{self.user.user_name}: {self.content}"
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            # Generate slug from content, truncate to 50 characters
-            self.slug = slugify(self.content)[:50]
         super().save(*args, **kwargs)
+        self.process_hashtags()
+
+    def process_hashtags(self):
+        existing_hashtags = self.hashtags.all()
+
+        # Extract hashtags from content
+        hashtags = re.findall(r'#\w+', self.content)
+
+        # Remove the '#' symbol from the tag
+        for tag_text in hashtags:
+            tag_name = tag_text[1:]
+
+            # Check if the tag already exists
+            existing_tag = existing_hashtags.filter(tag=tag_name).first()
+
+            if not existing_tag:
+                # Get or create the hashtag
+                hashtag, _ = HashTag.objects.get_or_create(tag=tag_name)
+
+                # Associate the hashtag with the post
+                self.hashtags.add(hashtag)
 
     class Meta:
         verbose_name = _("Post")
