002155236c91e634541a6d021695afe4dedd6dd5
CalebBlion <kaleba647@gmail.com>
2023-07-14 22:22:28 +0300

    ADD: list, get methods

--- betenda_api/Articles/urls.py
+++ betenda_api/Articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Article_CUD_View
+from .views import Article_CUD_View, Article_Get_View, Article_List_View
 
 
 urlpatterns = [
-    path('article', Article_CUD_View.as_view(), name="article_create_update_delete_view")
+    path('article', Article_CUD_View.as_view(), name="article_create_update_delete_view"),
+    path('list', Article_List_View.as_view(), name="article_list_view"),
+    path('get/<slug:slug>', Article_Get_View.as_view(), name="article_get_view")
 ]
