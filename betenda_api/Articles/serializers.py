40f540347c69d9b63c4e7d915165a160db50bff9
CalebBlion <kaleba647@gmail.com>
2023-07-14 03:44:21 +0300

    Serializer to create and update articles

--- betenda_api/Articles/serializers.py
+++ betenda_api/Articles/serializers.py
@@ -1,22 +1,42 @@
-from rest_framework.serializers import Serializer
+from Users.models import User
+from Users.serializers import User_CUD_Serializer
+from rest_framework import serializers
 from .models import Article
 
 
-class ArticleSerializer(Serializer.ModelSerializer):
+
+class ArticleSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Article
         fields = [
             'title',
+            'slug',
             'desc',
             'body',
             'image',
             'authors',
             'status',
             'featured',
+            'published_date',
+            'modified_date',
         ]
+        extra_kwargs = {
+            'slug': {'read_only': True},
+            'published_date': {'read_only': True},
+            'modified_date': {'read_only': True},
+        }
 
     def create(self, validated_data):
-        return super().create(validated_data)
+        authors = validated_data.pop("authors", [])
+        instance = super().create(validated_data)
+        for author in authors:
+            instance.authors.add(author)
+        return instance
 
     def update(self, instance, validated_data):
-        return super().update(instance, validated_data)
+        authors = validated_data.pop("authors", [])
+        instance = super().update(instance, validated_data)
+        for author in authors:
+            instance.authors.add(author)
+        return instance
