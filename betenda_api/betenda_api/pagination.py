2d7f813c1c5268b0e72cc9b9ba4ecd46970e9f1f
CalebBlion <kaleba647@gmail.com>
2023-07-14 03:47:29 +0300

    added docstrings

--- betenda_api/betenda_api/pagination.py
+++ betenda_api/betenda_api/pagination.py
@@ -4,6 +4,9 @@ import math
 
 
 class LargeResultsSetPagination(PageNumberPagination):
+    '''
+    Get 100 posts at once and a max page size of 1000
+    '''
     page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 1000
@@ -22,10 +25,13 @@ DEFAULT_STANDARD_PAGE_SIZE = 20
 
 
 class StandardResultsSetPagination(PageNumberPagination):
+    '''
+    Get 20 posts at once and a max page size of 200
+    '''
     page = DEFAULT_STANDARD_PAGE
     page_size = DEFAULT_STANDARD_PAGE_SIZE
     page_size_query_param = 'page_size'
-    max_page_size = 500
+    max_page_size = 200
 
     def get_paginated_response(self, data):
         return Response({
