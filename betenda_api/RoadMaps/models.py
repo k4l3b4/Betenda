795fddf43ada2ea6a0aaa9573fc2d44756f39719
CalebBlion <kaleba647@gmail.com>
2023-07-15 23:45:54 +0300

    Changed RoadMap model to Goal model

--- betenda_api/RoadMaps/models.py
+++ betenda_api/RoadMaps/models.py
@@ -2,13 +2,29 @@ from django.db import models
 from django.utils.translation import gettext as _
 
 # Create your models here.
-class RoadMap(models.Model):
+
+
+class Goal(models.Model):
+    GOAL_TYPE = (
+        ('DEV', 'Development goal'),
+        ('COM', 'Community goal'),
+        ('GOYE', 'Civic goal'),
+    )
+
     goal_name = models.CharField(_("Goal name"), max_length=255)
-    goal_desc = models.CharField(_("Goal description"), max_length=255)
+    goal_desc = models.TextField(_("Goal description"))
+    goal_type = models.CharField(
+        _("Goal type"), choices=GOAL_TYPE, max_length=255)
     achieved = models.BooleanField(_("Goal achieved"), default=False)
-    goal_set = models.DateTimeField(_("Goal set on"), auto_now=False, auto_now_add=True)
-    goal_due = models.DateTimeField(_("Ideal achievement date"), auto_now=False, auto_now_add=False)
+    goal_set = models.DateTimeField(
+        _("Goal set on"), auto_now=False, auto_now_add=False)
+    goal_due = models.DateTimeField(
+        _("Ideal achievement date"), auto_now=False, auto_now_add=False)
+    canceled = models.BooleanField(_("Goal canceled"), default=False)
 
     def __str__(self):
         return f"{self.goal_name} due on {self.goal_due}, achieved: {self.achieved}"
     
+    class Meta:
+        verbose_name=_("Goal")
+        verbose_name_plural=_("Goals")
