f38cbe650af95190145a8b5b3fba4e6e0a43a764
CalebBlion <kaleba647@gmail.com>
2023-07-15 23:46:03 +0300

    Registered Goal model

--- betenda_api/RoadMaps/admin.py
+++ betenda_api/RoadMaps/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from RoadMaps.models import Goal
+
 # Register your models here.
+
+
+@admin.register(Goal)
+class Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "goal_name",
+        "goal_desc",
+        "achieved",
+        "canceled",
+        "goal_set",
+        "goal_due",
+    )
