98c676818e8c45376e56eb2c530caddba43e4029
CalebBlion <kaleba647@gmail.com>
2023-07-15 23:51:55 +0300

    get_reactions & added user and id fields and a mac length validator for the content

--- betenda_api/Posts/serializers.py
+++ betenda_api/Posts/serializers.py
@@ -1,30 +1,37 @@
+from Reactions.models import Reaction, ReactionCount
+from betenda_api.methods import get_reactions
+from rest_framework.fields import MaxLengthValidator
 from rest_framework import serializers
-
 from Posts.models import Post
 from Users.serializers import User_CUD_Serializer
+from django.contrib.contenttypes.models import ContentType
 
 
 class Post_CUD_Serializer(serializers.ModelSerializer):
-    user = User_CUD_Serializer()
+    user = User_CUD_Serializer(read_only=True)
+    reactions = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = [
-            'user',
+            'id',
             'content',
             'parent',
             'slug',
             'media',
             'media_type',
+            'user',
+            'reactions',
             'created_at',
             'edited_at',
         ]
         extra_kwargs = {
-            'user': {'read_only': True},
-            'parent': {'read_only': True},
-            'slug': {'read_only': True},
-            'created_at': {'read_only': True},
-            'edited_at': {'read_only': True},
+            'id': {'read_only': True},
+            'user': {'read_only': True, 'allow_null': True},
+            'slug': {'read_only': True, 'allow_null': True},
+            'content': {'validators': [MaxLengthValidator(300)]},
+            'created_at': {'read_only': True, 'allow_null': True},
+            'edited_at': {'read_only': True, 'allow_null': True},
         }
 
     def create(self, validated_data):
@@ -32,3 +39,6 @@ class Post_CUD_Serializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         return super().update(instance, validated_data)
+
+    def get_reactions(self, obj):
+        return get_reactions(self, obj)
