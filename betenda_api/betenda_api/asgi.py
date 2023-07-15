2eb464c8e0b2faf3fa947859e5d5cb97e6a86ddd
CalebBlion <kaleba647@gmail.com>
2023-07-15 23:23:41 +0300

    MOD: Added a websocket route

--- betenda_api/betenda_api/asgi.py
+++ betenda_api/betenda_api/asgi.py
@@ -1,13 +1,20 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from betenda_api.middleware.ChannelsAuthMiddleware import JWTAuthMiddleware
+from channels.routing import ProtocolTypeRouter, URLRouter
+from betenda_api.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Betenda.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'betenda_api.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": JWTAuthMiddleware(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
 })
