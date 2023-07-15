9460640bdd8bee7ea49d3a24698341602285ad80
CalebBlion <kaleba647@gmail.com>
2023-07-15 23:54:50 +0300

    feat: Added a follow, unfollow mechanism and also fixed a tiny bug

--- betenda_api/Users/views.py
+++ betenda_api/Users/views.py
@@ -1,10 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
+from rest_framework.decorators import action
 from rest_framework_simplejwt.views import TokenObtainPairView
-from Users.models import Device, Invitation
-from Users.serializers import User_CUD_Serializer, UserTokenSerializer
-from betenda_api.methods import BadRequest, PermissionDenied, send_response
+from Users.models import FollowerRequest, Invitation, UserProfile
+from Users.serializers import User_CUD_Serializer, UserProfileSerializer, UserTokenSerializer
+from betenda_api.methods import BadRequest, PermissionDenied, UselessRequest, save_notification, send_notification, send_response
 from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
 
 # Create your views here.
 
@@ -15,19 +15,26 @@ class UserCreateView(generics.CreateAPIView):
 
     def post(self, request, code, format=None):
         try:
-            invite = Invitation.objects.get(invitation_code=code)
+            invite = Invitation.objects.get(
+                invitation_code=code, disabled=False, expired=False)
         except:
-            raise PermissionDenied("You are not authorized to register an account")
-
-        serializer = User_CUD_Serializer(request.data)
+            raise PermissionDenied(
+                "You are not authorized to register an account")
 
         # Just in case the invitation instance wasn't deleted for some reason
-        if invite.count < 11:
+
+        if invite.count < 10:
+            serializer = User_CUD_Serializer(data=request.data)
             if serializer.is_valid():
-                serializer.save()
+                serializer.save(invited_by=invite.user)
+                invite.count = invite.count + 1
+                invite.save()
                 return send_response(serializer.data, "Account registered successfully")
             raise BadRequest(serializer.errors)
-        raise PermissionDenied("You are not authorized to register for an account")
+        invite.expired = True
+        invite.save()
+        raise PermissionDenied(
+            "Opps.. the invitation code has expired, guess someone beat you to it.")
 
 
 class LoginView(TokenObtainPairView):
@@ -40,7 +47,6 @@ class LoginView(TokenObtainPairView):
     #         validated_data = serializer.validate(serializer.validated_data)
     #         return send_response(validated_data, 'Authentication successful')
     #     raise BadRequest(serializer.errors)
-        
 
     # # Get the user's device information
     # device, created = Device.objects.get_or_create(
@@ -58,3 +64,54 @@ class LoginView(TokenObtainPairView):
 
     # if not device.logged_in:
     #     return send_response(None, 'We have just sent you an email, authenticate your self', 200)
+
+
+class UserProfileFollowAPIView(generics.GenericAPIView, mixins.UpdateModelMixin):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+    @action(detail=True, methods=['post'])
+    def follow(self, request, pk=None):
+        '''
+        follow the user that was requested
+        '''
+        user_profile = self.get_object()
+        user = request.user
+        if user_profile.user_id == user.id:
+            # Raise an exception if the user is trying to follow them selves
+            # NOTE: there should be no way to do this in the frontend but there is no such thing as being to secure so...
+            raise UselessRequest("Trying to follow your self isn't allowed😂")
+
+        if not user_profile.is_private:
+            # If the user profile is not private, add the follower directly
+            user_profile.followers.add(user)
+            user_profile.followers_count += 1
+            user_profile.save()
+            notification = save_notification(
+                user=user_profile, message=f"@{user.user_name} has started following you!")
+            send_notification(user_profile.user_id, notification)
+            return send_response('You are now following this user.')
+
+        # If the user profile is private, create a follower request
+        follower_request, created = FollowerRequest.objects.get_or_create(
+            user_profile=user_profile,
+            follower=user
+        )
+
+        if created:
+            notification = save_notification(
+                user=user_profile, message=f"@{user.user_name} has requested to follow you!")
+            send_notification(user_profile.user_id, notification)
+            return send_response('Your request to follow this user has been sent.')
+        else:
+            follower_request.delete()
+            # abstracted method to send a Response object
+            return send_response('Your follow request has been canceled')
+
+    @action(detail=True, methods=['post'])
+    def unfollow(self, request, pk=None):
+        user_profile = self.get_object()
+        user_profile.followers.remove(request.user)
+        user_profile.followers_count -= 1
+        user_profile.save()
+        return send_response('Your follow request has been canceled')
