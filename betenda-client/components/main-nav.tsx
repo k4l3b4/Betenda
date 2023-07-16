d38240470a0f6b4c2c415622aac113a000a470f8
CalebBlion <kaleba647@gmail.com>
2023-07-16 22:20:44 +0300

    Removed nav links

--- betenda-client/components/main-nav.tsx
+++ betenda-client/components/main-nav.tsx
@@ -3,8 +3,7 @@ import Link from "next/link"
 
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
+import SearchComp from "@/components/search/search"
 
 interface MainNavProps {
   items?: NavItem[]
@@ -12,30 +11,13 @@ interface MainNavProps {
 
 export function MainNav({ items }: MainNavProps) {
   return (
-    <div className="flex gap-6 md:gap-10">
+    <div className="flex w-full items-center gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
-      {items?.length ? (
-        <nav className="flex gap-6">
-          {items?.map(
-            (item, index) =>
-              item.href && (
-                <Link
-                  key={index}
-                  href={item.href}
-                  className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground",
-                    item.disabled && "cursor-not-allowed opacity-80"
-                  )}
-                >
-                  {item.title}
-                </Link>
-              )
-          )}
-        </nav>
-      ) : null}
+      <div className="w-full">
+        <SearchComp redirect="search" />
+      </div>
     </div>
   )
 }
