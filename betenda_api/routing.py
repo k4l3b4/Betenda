5cee0c774783ead13cf2c579e011f9428b8372ca
CalebBlion <kaleba647@gmail.com>
2023-07-19 23:01:52 +0300

    Comment consumer

--- betenda_api/routing.py
+++ betenda_api/routing.py
@@ -1,6 +1,8 @@
 from django.urls import re_path
+from Comments.consumers import CommentConsumer
 from Notifications.consumers import NotificationConsumer
 
 websocket_urlpatterns = [
+    re_path(r'ws/comment/$', CommentConsumer.as_asgi(), name="comments_create_update_delete_consumer"),
     re_path(r'ws/notifications/$', NotificationConsumer.as_asgi(), name="notifications_get_read_consumer"),
 ]
