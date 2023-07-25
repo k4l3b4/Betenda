a0751c494eeb8649170b3d969b854bdbb09c961d
CalebBlion <kaleba647@gmail.com>
2023-07-25 23:13:43 +0300

    Updated model

--- Notifications/models.py
+++ Notifications/models.py
@@ -3,9 +3,20 @@ from django.utils.translation import gettext as _
 
 
 class Notification(models.Model):
+    TYPE = (
+        ('SYSTEM', 'System'),
+        ('REPLIES', 'Draft'),
+        ('COMMENTS', 'Published'),
+        ('FRIENDS', 'Published'),
+        ('MENTIONS', 'Mentions'),
+        ('ACHIEVEMENTS', 'Published'),
+    )
+
     user = models.ForeignKey('Users.User', verbose_name=_(
         "User"), on_delete=models.CASCADE)
     message = models.TextField(_("Message"))
+    message_type = models.CharField(
+        _("Message type"), choices=TYPE, default="SYSTEM", max_length=50)
     is_read = models.BooleanField(_("Is read"), default=False)
     created_at = models.DateTimeField(_("Created date"), auto_now_add=True)
 
@@ -19,3 +30,4 @@ class Notification(models.Model):
     class Meta:
         verbose_name = _("Notification")
         verbose_name_plural = _("Notifications")
+        ordering=('-created_at',)
