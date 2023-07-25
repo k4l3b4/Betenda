f7879cc829b059cbb45f70c9f24c754c5bd18805
CalebBlion <kaleba647@gmail.com>
2023-07-25 23:14:52 +0300

    Updated consumer logic

--- Notifications/consumers.py
+++ Notifications/consumers.py
@@ -1,4 +1,5 @@
 from Notifications.models import Notification
+from .serializers import NotificationSerializer
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
@@ -14,15 +15,18 @@ class NotificationConsumer(AsyncWebsocketConsumer):
             self.group_name,
             self.channel_name
         )
-        await self.channel_layer.group_add(str(user_id), self.channel_name)
+        await self.send(text_data=json.dumps("Notifications socket connection established!"))
 
     async def disconnect(self, close_code):
         user_id = self.scope["user"].id
-        await self.channel_layer.group_discard(str(user_id), self.channel_name)
+        await self.channel_layer.group_discard(f'notification_{str(user_id)}', self.channel_name)
 
     async def notify(self, event):
-        message = event["message"]
-        await self.send(text_data=json.dumps(message))
+        notification_data = event.get("object")  # Access the "object" key from the event data
+        if notification_data:
+            serializer = NotificationSerializer(notification_data)
+            notification_json = serializer.data
+        await self.send(text_data=json.dumps(notification_json))
 
     async def receive(self, text_data=None, bytes_data=None):
         message = json.loads(text_data)
