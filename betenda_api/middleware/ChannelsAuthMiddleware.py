28d46e7f1f032e7d295e5f66fac21fb32e7f162b
CalebBlion <kaleba647@gmail.com>
2023-07-27 01:10:50 +0300

    Update the Channel auth middleware

--- betenda_api/middleware/ChannelsAuthMiddleware.py
+++ betenda_api/middleware/ChannelsAuthMiddleware.py
@@ -9,10 +9,12 @@ from rest_framework_simplejwt.tokens import UntypedToken
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from jwt import decode as jwt_decode
 
+
 class JWTAuthMiddleware:
     """
     Custom token auth middleware
     """
+
     def __init__(self, inner):
         # Store the ASGI application we were passed
         self.inner = inner
@@ -23,33 +25,29 @@ class JWTAuthMiddleware:
         close_old_connections()
 
         # Get the token
-        token = parse_qs(scope["query_string"].decode("utf8"))["token"][0]
+        try:
+            token = parse_qs(scope["query_string"].decode("utf8"))["token"][0]
+        except:
+            token = None
 
         # Try to authenticate the user
-        try:
-            # This will automatically validate the token and raise an error if the token is invalid
-            UntypedToken(token)
-        except (InvalidToken, TokenError) as e:
-            # Token is invalid
-            print(e)
-            return None
-        else:
-            # Then token is valid, decode it
-            decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-            print(decoded_data)
-            # Will return a dictionary like -
-            # {
-            #     "token_type": "access",
-            #     "exp": 1568770772,
-            #     "jti": "5c15e80d65b04c20ad34d77b6703251b",
-            #     "user_id": 6
-            # }
+        if token:
+            try:
+                # Validate the token and raise an error if the token is invalid
+                UntypedToken(token)
+            except (InvalidToken, TokenError) as e:
+                # Token is invalid
+                print(e)
+                return None
+            else:
+                # Then token is valid, decode it
+                decoded_data = jwt_decode(
+                    token, settings.SECRET_KEY, algorithms=["HS256"])
+                # Get the user
+                user = await self.get_user(decoded_data["user_id"])
 
-            # Get the user using ID
-            user = await self.get_user(decoded_data["user_id"])
-
-        # Return the inner application directly and let it run everything else
-        return await self.inner(dict(scope, user=user), receive, send)
+            return await self.inner(dict(scope, user=user), receive, send)
+        return None
 
     @database_sync_to_async
     def get_user(self, user_id):
