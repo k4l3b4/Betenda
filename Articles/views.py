55a1121925a39716be5f01f3c55b5c1fe1f87400
CalebBlion <kaleba647@gmail.com>
2023-07-27 02:03:32 +0300

    Fixed

--- Articles/views.py
+++ Articles/views.py
@@ -19,14 +19,11 @@ class Article_Get_View(generics.RetrieveAPIView):
 
     def get(self, request, slug, *args, **kwargs):
         try:
-            try:
-                article = self.queryset.get(slug=slug)
-            except:
-                raise ResourceNotFound("The article was not found")
-            serializer = ArticleGetSerializer(article)
-            return send_response(serializer.data, "Article retrieved successfully")
+            article = self.queryset.get(slug=slug)
         except:
-          return ServerError("There was an error on our side")
+            raise ResourceNotFound("The article was not found")
+        serializer = ArticleGetSerializer(article)
+        return send_response(serializer.data, "Article retrieved successfully")
 
 
 class Article_CUD_View(APIView):
@@ -37,109 +34,100 @@ class Article_CUD_View(APIView):
     serializer_class = ArticleSerializer
 
     def post(self, request, *args, **kwargs):
-        try:
-            user = request.user
-            user_id = str(user.id)
-            data = request.data.copy()
-            authors = data.get('authors', None)
-            if authors:
-                authors_list = [user_id] + [author.strip()
-                                            for author in authors.split(',')]
-            else:
-                authors_list = None
-            groups = ['Admin']
-            perms = ['Articles.add_article']
-            passable_data = {
-                "title": data.get('title', None),
-                "desc": data.get('desc', None),
-                "body": data.get('body', None),
-                "image": data.get('image', None),
-                "status": data.get('status', None),
-                "featured": data.get('featured', None),
-                "authors": authors_list,
-            }
-            if check_user_permissions(user=user, groups=groups, perms=perms):
-                print(passable_data)
-                serializer = ArticleSerializer(data=passable_data)
-                if not serializer.is_valid():
-                    raise BadRequest(serializer.errors)
-                serializer.save()
-                return send_response(serializer.data, "Article created successfully", 201)
-            raise PermissionDenied("You don't have permission for this action")
-        except:
-          return ServerError("There was an error on our side")
+        user = request.user
+        user_id = str(user.id)
+        data = request.data.copy()
+        authors = data.get('authors', None)
+        if authors:
+            authors_list = [user_id] + [author.strip()
+                                        for author in authors.split(',')]
+        else:
+            authors_list = None
+        groups = ['Admin']
+        perms = ['Articles.add_article']
+        passable_data = {
+            "title": data.get('title', None),
+            "desc": data.get('desc', None),
+            "body": data.get('body', None),
+            "image": data.get('image', None),
+            "status": data.get('status', None),
+            "featured": data.get('featured', None),
+            "authors": authors_list,
+        }
+        if check_user_permissions(user=user, groups=groups, perms=perms):
+            print(passable_data)
+            serializer = ArticleSerializer(data=passable_data)
+            if not serializer.is_valid():
+                raise BadRequest(serializer.errors)
+            serializer.save()
+            return send_response(serializer.data, "Article created successfully", 201)
+        raise PermissionDenied("You don't have permission for this action")
 
 
     def patch(self, request, *args, **kwargs):
+        user = request.user
+        user_id = str(user.id)
+        data = request.data.copy()
+        authors = request.data.get('authors', None)
+        if authors:
+            authors_list = [user_id] + [author.strip()
+                                        for author in authors.split(',')]
+        else:
+            authors_list = None
+        groups = ['Admin']
+        perms = ['Articles.change_article']
+        passable_data = {
+            "title": data.get('title', None),
+            "desc": data.get('desc', None),
+            "body": data.get('body', None),
+            "image": data.get('image', None),
+            "status": data.get('status', None),
+            "featured": data.get('featured', None),
+            "authors": authors_list,
+        }
+        passable_data = {
+            key: value
+            for key, value in passable_data.items()
+            if value is not None
+        }
         try:
-            user = request.user
-            user_id = str(user.id)
-            data = request.data.copy()
-            authors = request.data.get('authors', None)
-            if authors:
-                authors_list = [user_id] + [author.strip()
-                                            for author in authors.split(',')]
-            else:
-                authors_list = None
-            groups = ['Admin']
-            perms = ['Articles.change_article']
-            passable_data = {
-                "title": data.get('title', None),
-                "desc": data.get('desc', None),
-                "body": data.get('body', None),
-                "image": data.get('image', None),
-                "status": data.get('status', None),
-                "featured": data.get('featured', None),
-                "authors": authors_list,
-            }
-            passable_data = {
-                key: value
-                for key, value in passable_data.items()
-                if value is not None
-            }
-            try:
-                id = request.data['id']
-            except:
-                raise BadRequest(
-                    "Needed information was not included: resource ID")
-
-            try:
-                instance = Article.objects.get(id=id)
-            except:
-                raise ResourceNotFound("Article was not found")
-
-            # check if the user is the main author or is Admin/has permission to edit articles
-            if user.id == instance.authors.first().id or check_user_permissions(user=user, groups=groups, perms=perms):
-                serializer = ArticleSerializer(instance=instance,
-                                            data=passable_data, partial=True)
-                if not serializer.is_valid():
-                    raise BadRequest(serializer.errors)
-                serializer.save()
-                return send_response(serializer.data, "Article updated successfully")
-
-            raise PermissionDenied("You don't have permission for this action")
+            id = request.data['id']
         except:
-          return ServerError("There was an error on our side")
+            raise BadRequest(
+                "Needed information was not included: resource ID")
+
+        try:
+            instance = Article.objects.get(id=id)
+        except:
+            raise ResourceNotFound("Article was not found")
+
+        # check if the user is the main author or is Admin/has permission to edit articles
+        if user.id == instance.authors.first().id or check_user_permissions(user=user, groups=groups, perms=perms):
+            serializer = ArticleSerializer(instance=instance,
+                                        data=passable_data, partial=True)
+            if not serializer.is_valid():
+                raise BadRequest(serializer.errors)
+            serializer.save()
+            return send_response(serializer.data, "Article updated successfully")
+
+        raise PermissionDenied("You don't have permission for this action")
 
 
     def delete(self, request, *args, **kwargs): 
+        user = request.user
+        groups = ['Admin']
+        perms = ['Articles.delete_article']
         try:
-            user = request.user
-            groups = ['Admin']
-            perms = ['Articles.delete_article']
-            try:
-                id = request.data['id']
-            except:
-                raise BadRequest(
-                    "Needed information was not included: resource ID")
-            try:
-                instance = Article.objects.get(id=id)
-            except:
-                raise ResourceNotFound("Article was not found")
-            # check if the user is the main author or is Admin/has permission to delete articles
-            if instance.authors.first() and user.id == instance.authors.first().id or check_user_permissions(user=user, groups=groups, perms=perms):
-                instance.delete()
-                return send_response(None, "Article deleted successfully")
-            raise PermissionDenied("You don't have permission for this action")
+            id = request.data['id']
         except:
-          return ServerError("There was an error on our side")
+            raise BadRequest(
+                "Needed information was not included: resource ID")
+        try:
+            instance = Article.objects.get(id=id)
+        except:
+            raise ResourceNotFound("Article was not found")
+        # check if the user is the main author or is Admin/has permission to delete articles
+        if instance.authors.first() and user.id == instance.authors.first().id or check_user_permissions(user=user, groups=groups, perms=perms):
+            instance.delete()
+            return send_response(None, "Article deleted successfully")
+        raise PermissionDenied("You don't have permission for this action")
--- Comments/views.py
+++ Comments/views.py
@@ -14,86 +14,77 @@ class Comment_CUD_View(APIView):
     serializer_class = Comment_CUD_Serializer
 
     def post(self, request):
-        try:
-            resource_id = request.GET.get('resource_id')
-            resource_type = request.GET.get('resource_type')
-            parent_id = request.GET.get('parent_id')
+        resource_id = request.GET.get('resource_id')
+        resource_type = request.GET.get('resource_type')
+        parent_id = request.GET.get('parent_id')
 
-            user = request.user
+        user = request.user
 
-            # rases exception if key isn't present or is empty ""
-            validate_key_value(resource_id, "resource_id")
+        # rases exception if key isn't present or is empty ""
+        validate_key_value(resource_id, "resource_id")
 
-            if parent_id & parent_id != "":
-                try:
-                    parent = Comment.objects.get(id=parent_id)
-                except:
-                    raise ResourceNotFound("Parent comment not found")
-            else:
-                parent = None
+        if parent_id & parent_id != "":
+            try:
+                parent = Comment.objects.get(id=parent_id)
+            except:
+                raise ResourceNotFound("Parent comment not found")
+        else:
+            parent = None
 
-            if resource_type == "article":
-                try:
-                    instance = Article.objects.get(id=resource_id)
-                except:
-                    raise ResourceNotFound("Article was not found")
+        if resource_type == "article":
+            try:
+                instance = Article.objects.get(id=resource_id)
+            except:
+                raise ResourceNotFound("Article was not found")
 
-                content_type = ContentType.objects.get_for_model(instance)
+            content_type = ContentType.objects.get_for_model(instance)
 
-                serializer = self.serializer_class(data=request.data)
-                if serializer.is_valid():
-                    serializer.save(content_object=instance, user=user,
-                                    content_type=content_type, parent=parent)
-                    return send_response(serializer.data, "Commented successfully", 201)
-                raise BadRequest(serializer.errors)
-            raise BadRequest("Needed information was not included: resource_type")
-        except:
-          return ServerError("There was an error on our side")
+            serializer = self.serializer_class(data=request.data)
+            if serializer.is_valid():
+                serializer.save(content_object=instance, user=user,
+                                content_type=content_type, parent=parent)
+                return send_response(serializer.data, "Commented successfully", 201)
+            raise BadRequest(serializer.errors)
+        raise BadRequest("Needed information was not included: resource_type")
 
     def patch(self, request):
+        comment_id = request.GET.get('comment_id')
+        user = request.user
+
+        validate_key_value(comment_id, "comment_id")
+
         try:
-            comment_id = request.GET.get('comment_id')
-            user = request.user
-
-            validate_key_value(comment_id, "comment_id")
-
-            try:
-                instance = Comment.objects.get(id=comment_id)
-            except:
-                raise ResourceNotFound("The resource not found")
-
-            serializer = self.serializer_class(
-                instance=instance, data=request.data, partial=True)
-            if instance.user.id == user.id:
-                if serializer.is_valid():
-                    serializer.save()
-                    return send_response(serializer.data, "Comment updated successfully")
-                raise BadRequest(serializer.errors)
-            raise PermissionDenied(
-                "You are not allowed to perform this action")
+            instance = Comment.objects.get(id=comment_id)
         except:
-          return ServerError("There was an error on our side")
+            raise ResourceNotFound("The resource not found")
+
+        serializer = self.serializer_class(
+            instance=instance, data=request.data, partial=True)
+        if instance.user.id == user.id:
+            if serializer.is_valid():
+                serializer.save()
+                return send_response(serializer.data, "Comment updated successfully")
+            raise BadRequest(serializer.errors)
+        raise PermissionDenied(
+            "You are not allowed to perform this action")
 
 
     def delete(self, request):
+        comment_id = request.GET.get('comment_id')
+        user = request.user
+
+        validate_key_value(comment_id, "comment_id")
+
         try:
-            comment_id = request.GET.get('comment_id')
-            user = request.user
-
-            validate_key_value(comment_id, "comment_id")
-
-            try:
-                instance = Comment.objects.get(id=comment_id)
-            except:
-                raise ResourceNotFound("The resource not found")
-            
-            if instance.user.id == user.id:
-                instance.delete()
-                return send_response(None, "Comment deleted successfully")
-            raise PermissionDenied(
-                "You are not allowed to perform this action")
+            instance = Comment.objects.get(id=comment_id)
         except:
-          return ServerError("There was an error on our side")
+            raise ResourceNotFound("The resource not found")
+        
+        if instance.user.id == user.id:
+            instance.delete()
+            return send_response(None, "Comment deleted successfully")
+        raise PermissionDenied(
+            "You are not allowed to perform this action")
 
 
 
@@ -103,22 +94,19 @@ class Comment_List_View(generics.ListAPIView):
     pagination_class = StandardResultsSetPagination
 
     def get(self, request, *args, **kwargs):
-        try:
-            resource_id = request.GET.get('resource_id')
-            resource_type = request.GET.get('resource_type')
-            parent_id = request.GET.get('parent_id', None)
+        resource_id = request.GET.get('resource_id')
+        resource_type = request.GET.get('resource_type')
+        parent_id = request.GET.get('parent_id', None)
 
-            validate_key_value(resource_id, "resource_id")
-            validate_key_value(resource_type, "resource_type")
+        validate_key_value(resource_id, "resource_id")
+        validate_key_value(resource_type, "resource_type")
 
-            comments = self.queryset.filter(
-                content_type__model=resource_type, object_id=resource_id, parent_id=parent_id)
+        comments = self.queryset.filter(
+            content_type__model=resource_type, object_id=resource_id, parent_id=parent_id)
 
-            # Manually apply pagination
-            paginator = self.pagination_class()
-            paginated_comments = paginator.paginate_queryset(comments, request)
+        # Manually apply pagination
+        paginator = self.pagination_class()
+        paginated_comments = paginator.paginate_queryset(comments, request)
 
-            serializer = Comment_GET_Serializer(paginated_comments, many=True)
-            return paginator.get_paginated_response(serializer.data)
-        except:
-          return ServerError("There was an error on our side")
+        serializer = Comment_GET_Serializer(paginated_comments, many=True)
+        return paginator.get_paginated_response(serializer.data)
