d2eb44c7f428df326b881b85799058fa8c24caa1
CalebBlion <kaleba647@gmail.com>
2023-07-26 23:37:18 +0300

    Add the newly created view methods in the urls

--- Posts/urls.py
+++ Posts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from Posts.views import Post_CUD_View, Post_List_View
+from Posts.views import Post_CUD_View, Post_List_GET_View
 
 
 urlpatterns = [
-    path('get_posts', Post_List_View.as_view(), name="post_list_view"),
-    path('post', Post_CUD_View.as_view(), name="post_create_update_delete_view"),
+    path('get_posts', Post_List_GET_View.as_view(
+        {'get': 'list'}), name="post_list_view"),
+    path('posts_by_tag', Post_List_GET_View.as_view(
+        {'get': 'get_post_with_tag'}), name="post_filter_with_tag_view"),
+    path('get_post/<slug:slug>', Post_List_GET_View.as_view(
+        {'get': 'get_post'}), name="get_replies_by_slug"),
+    path('get_replies', Post_List_GET_View.as_view(
+        {'get': 'get_post_replies'}), name="get_replies_by_parent_id"),
+    path('post', Post_CUD_View.as_view(),
+         name="post_create_update_delete_view"),
 ]
