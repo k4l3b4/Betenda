a18353346b3f21c8bc2b47996392a5641be23ae4
CalebBlion <kaleba647@gmail.com>
2023-07-27 18:14:29 +0300

    Added 4 methods for querying posts

--- Posts/views.py
+++ Posts/views.py
@@ -1,18 +1,79 @@
 from betenda_api.pagination import StandardResultsSetPagination
-from rest_framework import generics
+from rest_framework import  viewsets
 from rest_framework.views import APIView
 from betenda_api.methods import BadRequest, PermissionDenied, ResourceNotFound, send_response, validate_key_value
+from rest_framework.decorators import action
 from .models import Post
-from .serializers import Post_CUD_Serializer
+from .serializers import Post_CUD_Serializer, Thread_CUD_Serializer
 
 
-class Post_List_View(generics.ListAPIView):
+class Post_List_GET_View(viewsets.ModelViewSet):
     '''
     Post list view
     '''
-    queryset = Post.objects.filter(parent=None)
-    serializer_class = Post_CUD_Serializer
-    pagination_class = StandardResultsSetPagination
+    queryset = Post.objects.filter(parent=None).select_related('user')
+
+    @action(detail=True, methods=['get'])
+    def list(self, request):
+        posts = self.queryset.all().prefetch_related('reactions', 'post_parent')
+        serializer = Post_CUD_Serializer(
+            posts, many=True, context={'request': request})
+        pagination_class = StandardResultsSetPagination()
+        paginated_posts = pagination_class.paginate_queryset(
+            serializer.data, request)
+
+        return pagination_class.get_paginated_response(paginated_posts)
+
+    @action(detail=True, methods=['get'])
+    def get_post(self, request, slug):
+        # Raises an exception if the key isn't present or is an empty string
+
+        # Filter posts with the given tag name in the hashtags ManyToMany field
+        try:
+            posts = Post.objects.get(
+                slug=slug)
+        except:
+            raise ResourceNotFound("The post was not found")
+        
+        serializer = Thread_CUD_Serializer(
+            posts, context={'request': request})
+
+        return send_response(serializer.data, "Post retrieved successfully")
+
+    @action(detail=True, methods=['get'])
+    def get_post_with_tag(self, request):
+        tag = request.GET.get('tag')
+        # Raises an exception if the key isn't present or is an empty string
+        validate_key_value(tag, "Tag")
+
+        # Filter posts with the given tag name in the hashtags ManyToMany field
+        posts = self.queryset.prefetch_related(
+            'reactions', 'post_parent', 'hashtags').filter(hashtags__tag=tag)
+        serializer = Post_CUD_Serializer(
+            posts, many=True, context={'request': request})
+        # Apply pagination to the queryset using StandardPagination
+        pagination_class = StandardResultsSetPagination()
+        paginated_posts = pagination_class.paginate_queryset(
+            serializer.data, request)
+        return pagination_class.get_paginated_response(paginated_posts)
+
+    @action(detail=True, methods=['get'])
+    def get_post_replies(self, request):
+        parent = request.GET.get('parent')
+        # Raises an exception if the key isn't present or is an empty string
+        validate_key_value(parent, "parent ID")
+
+        # Filter posts with the given tag name in the hashtags ManyToMany field
+        posts = Post.objects.filter(parent=parent).prefetch_related(
+            'reactions', 'post_parent', 'hashtags')
+        serializer = Post_CUD_Serializer(
+            posts, many=True, context={'request': request})
+        # Apply pagination to the queryset using StandardPagination
+        pagination_class = StandardResultsSetPagination()
+        paginated_posts = pagination_class.paginate_queryset(
+            serializer.data, request)
+        return pagination_class.get_paginated_response(paginated_posts)
+    # an algorism to query the feed would be great
 
 
 class Post_CUD_View(APIView):
