94b690ea0c3173ba3aa35c423c2d74aa14730a7f
CalebBlion <kaleba647@gmail.com>
2023-08-01 03:49:32 +0300

    Added List and Notifications serializer for Articles

--- Articles/serializers.py
+++ Articles/serializers.py
@@ -24,6 +24,7 @@ class ArticleSerializer(serializers.ModelSerializer):
         ]
         extra_kwargs = {
             'id': {'read_only': True},
+            'authors': {'allow_null': True},
             'slug': {'read_only': True},
             'body': {'write_only': True},
             'published_date': {'read_only': True},
@@ -84,3 +85,55 @@ class ArticleGetSerializer(serializers.ModelSerializer):
             content_type=content_type, object_id=obj.id).count()
 
         return comments_count
+
+
+# same serializer with only the body omitted since it will be to big to just leave it when unused
+class ArticleListSerializer(serializers.ModelSerializer):
+    authors = User_CUD_Serializer(many=True, read_only=True)
+    reactions = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'title',
+            'slug',
+            'desc',
+            'image',
+            'authors',
+            'status',
+            'comments_count',
+            'featured',
+            'published_date',
+            'modified_date',
+            'reactions'
+        ]
+
+    def get_reactions(self, obj):
+        return get_reactions_method(self, obj)
+    
+    def get_comments_count(self, obj):
+        '''
+        takes in context and obj to retrieve reactions for the specific objects
+        '''
+        comments_count = 0    
+        content_type = ContentType.objects.get_for_model(obj)
+
+        comments_count = Comment.objects.filter(
+            content_type=content_type, object_id=obj.id).count()
+
+        return comments_count
+    
+
+
+class Article_Notification_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'title',
+            'slug',
+            'image',
+            'status',
+        ]
