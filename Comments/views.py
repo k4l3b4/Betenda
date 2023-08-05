7b91f977b0c78b90c6f6febda489c7fd67b7323f
CalebBlion <kaleba647@gmail.com>
2023-08-05 23:42:00 +0300

    Feature: created comments and replies system

--- Comments/views.py
+++ Comments/views.py
@@ -1,7 +1,7 @@
 from django.contrib.contenttypes.models import ContentType
-from betenda_api.methods import ServerError
-from rest_framework.views import APIView
-from rest_framework import generics
+from Contributions.models import Poem, Saying
+from rest_framework.decorators import action
+from rest_framework import generics, viewsets
 from .models import Comment
 from .serializers import Comment_CUD_Serializer, Comment_GET_Serializer
 from betenda_api.pagination import StandardResultsSetPagination
@@ -10,44 +10,96 @@ from Articles.models import Article
 # Create your views here.
 
 
-class Comment_CUD_View(APIView):
+class Comment_CUD_View(viewsets.ModelViewSet):
     serializer_class = Comment_CUD_Serializer
 
-    def post(self, request):
+
+    @action(detail=True, methods=['post'])
+    def comment(self, request):
         resource_id = request.GET.get('resource_id')
         resource_type = request.GET.get('resource_type')
-        parent_id = request.GET.get('parent_id')
 
         user = request.user
 
         # rases exception if key isn't present or is empty ""
         validate_key_value(resource_id, "resource_id")
+        validate_key_value(resource_type, "resource_type")
 
-        if parent_id & parent_id != "":
-            try:
-                parent = Comment.objects.get(id=parent_id)
-            except:
-                raise ResourceNotFound("Parent comment not found")
-        else:
-            parent = None
+        # Commentable types
+        resource_type_mapping = {
+            "article": Article,
+            "saying": Saying,
+            "poem": Poem,
+        }
 
-        if resource_type == "article":
-            try:
-                instance = Article.objects.get(id=resource_id)
-            except:
-                raise ResourceNotFound("Article was not found")
+        try:
+            # trying to get the resource object
+            model_class = resource_type_mapping[resource_type]
+            instance = model_class.objects.get(id=resource_id)
+        except KeyError:
+            raise BadRequest("Invalid resource_type")
+        except model_class.DoesNotExist:
+            raise ResourceNotFound(
+                f"{resource_type.capitalize()} was not found")
 
-            content_type = ContentType.objects.get_for_model(instance)
+        content_type = ContentType.objects.get_for_model(instance)
 
-            serializer = self.serializer_class(data=request.data)
-            if serializer.is_valid():
-                serializer.save(content_object=instance, user=user,
-                                content_type=content_type, parent=parent)
-                return send_response(serializer.data, "Commented successfully", 201)
-            raise BadRequest(serializer.errors)
-        raise BadRequest("Needed information was not included: resource_type")
+        serializer = self.serializer_class(data=request.data, context = {'request': request})
+        if serializer.is_valid():
+            serializer.save(content_object=instance, user=user,
+                            content_type=content_type)
+            return send_response(serializer.data, "Commented successfully", 201)
+        raise BadRequest(serializer.errors)
+    
 
-    def patch(self, request):
+    @action(detail=True, methods=['post'])
+    def reply(self, request):
+        resource_id = request.GET.get('resource_id')
+        resource_type = request.GET.get('resource_type')
+        parent = request.GET.get('parent')
+
+        user = request.user
+
+        # rases exception if key isn't present or is empty ""
+        validate_key_value(resource_id, "resource_id")
+        validate_key_value(resource_type, "resource_type")
+        validate_key_value(parent, "parent_id")
+
+        # Comment repliable types
+        resource_type_mapping = {
+            "article": Article,
+            "saying": Saying,
+            "poem": Poem,
+        }
+        try:
+          parent_comment = Comment.objects.get(id=parent)
+          top_comment = parent_comment.parent if parent_comment.parent else parent_comment
+        except:
+          raise ResourceNotFound("Comment not found")
+
+        try:
+            # trying to get the resource object
+            model_class = resource_type_mapping[resource_type]
+            instance = model_class.objects.get(id=resource_id)
+        except KeyError:
+            raise BadRequest("Invalid resource_type")
+        except model_class.DoesNotExist:
+            raise ResourceNotFound(
+                f"{resource_type.capitalize()} was not found")
+
+        content_type = ContentType.objects.get_for_model(instance)
+            
+        serializer = self.serializer_class(data=request.data, context = {'request':request})
+        if serializer.is_valid():
+            serializer.save(content_object=instance, user=user,
+                            content_type=content_type, parent=top_comment, immediate_parent=parent_comment)
+            # reformat the returned data to fit in the frontend
+            return_serializer = Comment_GET_Serializer(serializer.instance, context = {'request': request})
+            return send_response(return_serializer.data, "Replied successfully", 201)
+        raise BadRequest(serializer.errors)
+    
+    @action(detail=True, methods=['patch'])
+    def update(self, request):
         comment_id = request.GET.get('comment_id')
         user = request.user
 
@@ -59,7 +111,7 @@ class Comment_CUD_View(APIView):
             raise ResourceNotFound("The resource not found")
 
         serializer = self.serializer_class(
-            instance=instance, data=request.data, partial=True)
+            instance=instance, data=request.data, partial=True, context = {'request':request})
         if instance.user.id == user.id:
             if serializer.is_valid():
                 serializer.save()
@@ -68,7 +120,7 @@ class Comment_CUD_View(APIView):
         raise PermissionDenied(
             "You are not allowed to perform this action")
 
-
+    @action(detail=True, methods=['delete'])
     def delete(self, request):
         comment_id = request.GET.get('comment_id')
         user = request.user
@@ -102,11 +154,11 @@ class Comment_List_View(generics.ListAPIView):
         validate_key_value(resource_type, "resource_type")
 
         comments = self.queryset.filter(
-            content_type__model=resource_type, object_id=resource_id, parent_id=parent_id)
+            content_type__model=resource_type, object_id=resource_id, parent=parent_id)
 
         # Manually apply pagination
         paginator = self.pagination_class()
         paginated_comments = paginator.paginate_queryset(comments, request)
 
-        serializer = Comment_GET_Serializer(paginated_comments, many=True)
+        serializer = Comment_GET_Serializer(paginated_comments, many=True, context = {'request':request})
         return paginator.get_paginated_response(serializer.data)
