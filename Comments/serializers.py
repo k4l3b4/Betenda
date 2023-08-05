1bf27aaaa775daa5f46d75e55f2b71cfa383f152
CalebBlion <kaleba647@gmail.com>
2023-08-05 23:41:12 +0300

    Updated serializer to match new schema and more

--- Comments/serializers.py
+++ Comments/serializers.py
@@ -1,15 +1,28 @@
+from django.db.models import Count
+from Users.serializers import User_SIMPLE_Serializer
 from betenda_api.methods import get_reactions_method
 from rest_framework import serializers
-from Users.serializers import User_CUD_Serializer
 from .models import Comment
 
 
 class Comment_CUD_Serializer(serializers.ModelSerializer):
+    user = User_SIMPLE_Serializer(read_only=True)
     class Meta:
         model = Comment
         fields = [
+            'id',
             'comment',
+            'user',
+            'parent',
+            'immediate_parent',
+            'created_at',
+            'updated_at',
         ]
+        extra_kwargs = {
+            'id':{'read_only':True},
+            'created_at':{'read_only':True},
+            'updated_at':{'read_only':True},
+        }
 
     def create(self, validated_data):
         return super().create(validated_data)
@@ -19,8 +32,10 @@ class Comment_CUD_Serializer(serializers.ModelSerializer):
 
 
 class Comment_GET_Serializer(serializers.ModelSerializer):
-    user = User_CUD_Serializer()
+    user = User_SIMPLE_Serializer()
     reactions = serializers.SerializerMethodField()
+    reply_to = serializers.SerializerMethodField()
+    reply_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
@@ -28,11 +43,46 @@ class Comment_GET_Serializer(serializers.ModelSerializer):
             'id',
             'comment',
             'user',
-            'parent_id',
+            'parent',
+            'immediate_parent',
+            'reply_to',
+            'reply_count',
             'reactions',
             'created_at',
             'updated_at',
         ]
+        extra_kwargs = {
+            'id':{'read_only':True},
+            'user':{'read_only':True},
+            'parent':{'read_only':True},
+            'immediate_parent':{'read_only':True},
+            'reactions':{'read_only':True},
+            'created_at':{'read_only':True},
+            'updated_at':{'read_only':True},
+        }
 
     def get_reactions(self, obj):
         return get_reactions_method(self, obj)
+    
+    def get_commenter(self, comment):
+        return comment.user.user_name if comment.user else None
+    
+    def get_reply_to(self, obj):
+        # Check if the comment has an immediate parent and that it is not a first level reply
+        if obj.immediate_parent and obj.immediate_parent != obj.parent:
+            return self.get_commenter(obj.immediate_parent)
+        else:
+            return None
+        
+    def get_reply_count(self, obj):
+        # Check if the comment has any replies
+        return Comment.objects.filter(parent=obj.id).count()
+
+class Comment_Notification_Serializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'comment',
+        ]
