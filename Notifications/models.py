f319350640a965b99bf6c3b64d43e8be7cd4ab9e
CalebBlion <kaleba647@gmail.com>
2023-08-05 23:46:10 +0300

    Updated schema

--- Notifications/models.py
+++ Notifications/models.py
@@ -3,20 +3,25 @@ from django.utils.translation import gettext as _
 
 
 class Notification(models.Model):
-    TYPE = (
-        ('SYSTEM', 'System'),
-        ('REPLIES', 'Draft'),
-        ('COMMENTS', 'Comments'),
-        ('FRIENDS', 'Friends'),
-        ('MENTIONS', 'Mentions'),
-    )
-
-    user = models.ForeignKey('Users.User', verbose_name=_(
-        "User"), on_delete=models.CASCADE)
+    TYPES = (
+            ("1", 'Like'),
+            ("2", 'Follow'),
+            ("3", 'Comment'),
+            ("4", 'Reply'),
+            ("5", 'Liked-Comment'),
+            ("6", 'Liked-Reply'), 
+            ("7", 'System')
+        )
     
+    sender = models.ForeignKey('Users.User', verbose_name=_("Sender"), on_delete=models.CASCADE, related_name='from_user', blank=True, null=True)
+    user = models.ForeignKey('Users.User', verbose_name=_("Recipient"), on_delete=models.CASCADE, related_name='to_user')
+    post = models.ForeignKey('Posts.Post', on_delete=models.CASCADE, related_name='notifiable_post', blank=True, null=True)
+    article = models.ForeignKey('Articles.Article', on_delete=models.CASCADE, related_name='notifiable_article', blank=True, null=True)
+    poem = models.ForeignKey('Contributions.Poem', on_delete=models.CASCADE, related_name='notifiable_article', blank=True, null=True)
+    comment = models.ForeignKey('Comments.Comment', on_delete=models.CASCADE, related_name='notifiable_article', blank=True, null=True)
     message = models.TextField(_("Message"))
     message_type = models.CharField(
-        _("Message type"), choices=TYPE, default="SYSTEM", max_length=50)
+        _("Message type"), choices=TYPES, default="7", max_length=50)
     is_read = models.BooleanField(_("Is read"), default=False)
     created_at = models.DateTimeField(_("Created date"), auto_now_add=True)
 
