1ba55355baf36c9446b62d358aeeaa3d7f2d6991
CalebBlion <kaleba647@gmail.com>
2023-08-13 01:14:25 +0300

    refactor: Swapped AutoSlugField for randomcharfield
    because content might be empty and handling that manually is just unnessesary i changed the field with a RandomCharField also from django_Extensions

--- Posts/models.py
+++ Posts/models.py
@@ -1,7 +1,8 @@
 import re
 from django.db import models
 from django.utils.translation import gettext as _
-from django_extensions.db.fields import AutoSlugField
+from filemime import filemime
+from django_extensions.db.fields import RandomCharField
 from django.contrib.contenttypes.fields import GenericRelation
 from HashTags.models import HashTag
 
@@ -13,16 +14,18 @@ class Post(models.Model):
         AUDIO = "AUDIO", "Audio"
 
     user = models.ForeignKey("Users.User", verbose_name=_(
-        "User"), on_delete=models.PROTECT, blank=True, null=True)
+        "User"), on_delete=models.PROTECT, blank=True, null=True, db_index=True)
     content = models.TextField(_("Content"), blank=True, db_index=True)
     parent = models.ForeignKey('self', verbose_name=_(
         "Replied to"), related_name="post_parent", on_delete=models.CASCADE, null=True, blank=True)
-    slug = AutoSlugField(_("Slug"), populate_from=['title'])
+    slug = RandomCharField(
+        _("Invitation code"), length=15, unique=True)
     media = models.FileField(_("Media"), upload_to='post/media/',
                              max_length=None, blank=True, null=True)
     hashtags = models.ManyToManyField(
         "HashTags.HashTag", blank=True, db_index=True)
     reactions = GenericRelation("Reactions.Reaction")
+    report = GenericRelation("Reports.Report")
     media_type = models.CharField(
         _("Media type"), max_length=20, choices=TYPE.choices, null=True, blank=True)
     created_at = models.DateTimeField(_("Created date"), auto_now_add=True)
@@ -35,6 +38,7 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         self.process_hashtags()
+        self.process_media_type()
 
     def process_hashtags(self):
         existing_hashtags = self.hashtags.all()
@@ -55,6 +59,22 @@ class Post(models.Model):
 
                 # Associate the hashtag with the post
                 self.hashtags.add(hashtag)
+
+    def process_media_type(self):
+        if self.media:
+            # Determine the file type using python-magic library
+            fileObj = filemime()
+            file_type = fileObj.load_file(
+                self.media.file.read(), mimeType=True)
+            if file_type:
+                # Set the media_type field based on the file type
+                if file_type.startswith("image"):
+                    self.media_type = self.TYPE.IMAGE
+                elif file_type.startswith("video"):
+                    self.media_type = self.TYPE.VIDEO
+                elif file_type.startswith("audio"):
+                    self.media_type = self.TYPE.AUDIO
+                self.save()
 
     class Meta:
         verbose_name = _("Post")
